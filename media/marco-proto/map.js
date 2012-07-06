225930a3d75b6656c51b06560bde766d0b592baf
Edwin Knuth <eknuth@ecotrust.org>
2012-07-05 18:37:31 -0700

    adding prototype

---
+++ media/marco-proto/map.js
@@ -0,0 +1,109 @@
+app.init = function () {
+
+    var map = new OpenLayers.Map(null, {
+        allOverlays: true,
+        displayProjection: new OpenLayers.Projection("EPSG:4326")
+    });
+    esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean Basemap", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13
+    });
+
+    map.addLayers([esriOcean]);
+    esriOcean.setZIndex(100);
+
+    map.addControl(new OpenLayers.Control.LayerSwitcher( {
+        roundedCorner: true
+    }));
+    map.addControl(new OpenLayers.Control.MousePosition({
+        element: document.getElementById('pos')
+    }));
+
+    map.events.register("moveend", null, function () {
+        // update the url when we move
+        app.updateUrl();
+    });
+
+    app.map = map;
+}
+
+app.addLayerToMap = function(layer) {
+    if (!layer.layer) {
+        var opts = {
+            displayInLayerSwitcher: false
+        };
+        if (layer.utfurl) {
+            layer.utfgrid = new OpenLayers.Layer.UTFGrid({
+
+                url: layer.utfurl,
+                                 
+                utfgridResolution: 4, // default is 2
+                displayInLayerSwitcher: false,
+                useJSONP: false
+            });
+            //layer.utfgrid.projection = new OpenLayers.Projection("EPSG:4326");  
+            app.map.addLayer(layer.utfgrid); 
+            
+            layer.utfcontrol = new OpenLayers.Control.UTFGrid({
+                layers: [layer.utfgrid],
+                handlerMode: 'click',
+                callback: function(infoLookup) {
+                    console.dir(infoLookup);
+                    //document.getElementById("info").innerHTML = msg;
+                }
+            });
+
+            app.map.addControl(layer.utfcontrol); 
+            	
+            layer.layer = new OpenLayers.Layer.XYZ(layer.name, 
+                //layer.type === 'XYZ' ? layer.url : layer.url + '.png', 
+                layer.url,
+                $.extend({}, opts, 
+                    {
+                        sphericalMercator: true
+                    }
+                )
+            );            
+            
+        } else if (layer.type == 'Vector') {
+            layer.layer = new OpenLayers.Layer.Vector(
+                layer.name,
+                {
+                    projection: new OpenLayers.Projection('EPSG:3857'),
+                    strategies: [new OpenLayers.Strategy.Fixed()],
+                    protocol: new OpenLayers.Protocol.HTTP({
+                        url: layer.url,
+                        format: new OpenLayers.Format.GeoJSON()
+                    })
+                }
+            );
+        } else { //if XYZ with no utfgrid
+            // adding layer to the map for the first time		
+            layer.layer = new OpenLayers.Layer.XYZ(layer.name, 
+                //layer.type === 'XYZ' ? layer.url : layer.url + '.png', 
+                layer.url,
+                $.extend({}, opts, 
+                    {
+                        sphericalMercator: true
+                    }
+                )
+            );
+        }
+        //layer.layer.projection = new OpenLayers.Projection("EPSG:3857");
+        app.map.addLayer(layer.layer);            
+    }
+    layer.layer.opacity = .3
+    layer.layer.setVisibility(true);
+}
+
+app.setLayerVisibility = function(layer, visibility) {
+    // if layer is in openlayers, hide it
+    if (layer.layer) {
+        layer.layer.setVisibility(visibility);
+    }
+}
+
+app.setLayerZIndex = function(layer, index) {
+    layer.layer.setZIndex(index);
+}
---
+++ media/marco-proto/models.js
@@ -0,0 +1,339 @@
+function layerModel(options, parent) {
+	var self = this;
+
+	// properties
+	self.id = options.id;
+	self.name = options.name;
+	self.url = options.url;
+	self.utfurl = options.utfurl || false; 
+	self.type = options.type;
+    self.legend = options.legend || false;
+    self.legendVisibility = ko.observable(false);
+    
+	// is the layer visible?
+	self.active = ko.observable(false);
+
+    self.activeSubLayer = ko.observable(false);
+	// store references to sublayers
+	self.subLayerIndexes = options.subLayers || [];
+	self.subLayers = function () {
+		return $.map(self.subLayerIndexes, function (layer_index) {
+			return app.viewModel.layerIndex[layer_index];
+		});
+	};
+
+	if (parent) {
+		// save a ref to the parent
+		self.parent = parent;
+	}
+    
+    self.toggleLegendVisibility = function() {
+        var layer = this;
+        layer.legendVisibility(!layer.legendVisibility());
+    }
+
+	self.deactivateLayer = function () {
+		var layer = this;
+		app.viewModel.activeLayers.remove(layer);
+		layer.active(false);
+        
+        app.setLayerVisibility(layer, false);
+        
+		if (layer.subLayerIndexes) {
+			$.each(layer.subLayers(), function (i, subLayer) {
+				if (subLayer.active()) {
+					subLayer.deactivateLayer();
+				}
+			});
+		}
+	};
+
+	self.activateLayer = function () {
+		var layer = this;
+        //factor out the following into an addLayerToMap function in map.js
+		app.addLayerToMap(layer);
+		// add it to the top of the active layers
+		app.viewModel.activeLayers.unshift(layer);
+		// set the active flag
+		layer.active(true);
+	};
+
+	self.toggleActive = function () {
+		var layer = this;
+		if (layer.active()) {
+			// layer is active
+			layer.deactivateLayer();
+			if (layer.parent) {
+				// layer has a parent
+				// turn off the parent shell layer
+				layer.parent.active(false);
+			}
+		} else {
+			// layer has a parent
+			if (layer.parent) {
+				// toggle sibling layers
+				if (layer.parent.type === 'radio') {
+					// only allow one sublayer on at a time
+					layer.parent.deactivateLayer();
+				}
+				// turn on the parent
+				layer.parent.active(true);
+			}
+			if (layer.subLayerIndexes.length > 0) {
+				// layer has sublayer, activate first layer
+				app.viewModel.layerIndex[layer.subLayerIndexes[0]].activateLayer();
+				layer.active(true);
+			} else {
+				// otherwise just activate the layer
+				layer.activateLayer();
+			}
+
+		}
+	};
+
+	self.raiseLayer = function (layer, event) {
+		var current = app.viewModel.activeLayers.indexOf(layer);
+		if (current === 0) {
+			// already at top
+			return;
+		}
+		$(event.target).closest('tr').fadeOut('fast', function () {
+			app.viewModel.activeLayers.remove(layer);
+			app.viewModel.activeLayers.splice(current - 1, 0, layer);
+		});
+
+	};
+
+	self.lowerLayer = function (layer, event) {
+		var current = app.viewModel.activeLayers.indexOf(layer);
+		if (current === app.viewModel.activeLayers().length) {
+			// already at top
+			return;
+		}
+		$(event.target).closest('tr').fadeOut('fast', function () {
+			app.viewModel.activeLayers.remove(layer);
+			app.viewModel.activeLayers.splice(current + 1, 0, layer);
+		});
+	};
+
+	self.isTopLayer = function (layer) {
+		return app.viewModel.activeLayers.indexOf(layer) === 0;
+	};
+
+	self.isBottomLayer = function (layer) {
+		return app.viewModel.activeLayers.indexOf(layer) === app.viewModel.activeLayers().length - 1;
+	}
+
+	// remove the layer dropdrown menu
+	self.closeMenu = function (layer, event) {
+		$(event.target).closest('.btn-group').removeClass('open');
+
+	}
+
+	return self;
+}
+
+function themeModel(name, layers) {
+	var self = this;
+
+	self.name = name;
+
+	// array of layers
+	self.layers = layers;
+
+	self.setActiveTheme = function (theme) {
+		if (self.isActiveTheme(theme)) {
+			app.viewModel.activeTheme(null);
+		} else {
+			app.viewModel.activeTheme(theme);
+		}
+	};
+
+	self.isActiveTheme = function (theme) {
+		return app.viewModel.activeTheme() === theme;
+	};
+
+	return self;
+}
+
+function bookmarkModel($popover) {
+	var self = this;
+	// name of the bookmark
+	self.bookmarkName = ko.observable();
+
+	// list of bookmarks
+	self.bookmarksList = ko.observableArray();
+
+	// load state from bookmark
+	self.loadBookmark = function (bookmark) {
+		// save the restore state
+		app.restoreState = app.getState();
+		
+		app.loadState(bookmark.state);
+
+		// show the alert for resting state
+		app.viewModel.error("restoreState");
+		$('#bookmark-popover').hide();
+
+	}
+
+	self.restoreState = function () {
+		// hide the error
+		app.viewModel.error(null);
+		// restore the state
+		app.loadState(app.restoreState);
+	}
+
+	self.removeBookmark = function (bookmark) {
+		self.bookmarksList.remove(bookmark);
+		$('#bookmark-popover').hide();
+		// store the bookmarks
+		self.storeBookmarks();
+	}
+
+	// handle the bookmark submit
+	self.saveBookmark = function () {
+		// add to the list of bookmarks
+		self.bookmarksList.unshift({
+			state: app.getState(),
+			name: self.bookmarkName()
+		});
+		$('#bookmark-popover').hide();
+		// store the bookmarks
+		self.storeBookmarks();
+	};
+
+	// get the url from a bookmark
+	self.getUrl = function (bookmark) {
+		return "#" + $.param(bookmark.state);
+	};
+
+	// store the bookmarks to local storage or server
+	self.storeBookmarks = function () {
+		localStorage.setItem("marco-bookmarks", JSON.stringify(self.bookmarksList()));
+	};
+
+	// method for loading existing bookmarks
+	self.getBookmarks = function () {
+		var existingBookmarks = localStorage.getItem("marco-bookmarks");
+		if (existingBookmarks) {
+			self.bookmarksList = ko.observableArray(JSON.parse(existingBookmarks));
+		}
+	}
+
+	self.cancel = function () {
+		$('#bookmark-popover').hide();
+	}
+
+	// load the bookmarks
+	self.getBookmarks();
+
+	return self;
+}
+
+
+function viewModel() {
+	var self = this;
+
+	// list of active layermodels
+	self.activeLayers = ko.observableArray();
+
+	// reference to active theme model
+	self.activeTheme = ko.observable();
+
+	// list of theme models
+	self.themes = [];
+
+	// index for filter autocomplete and lookups
+	self.layerIndex = {};
+
+	// viewmodel for bookmarks
+	self.bookmarks = new bookmarkModel();
+
+	// set the error type
+	// can be one of:
+	// 	restoreState
+	self.error = ko.observable();
+    
+    //show Legend by default
+    self.showLegend = ko.observable(true);
+    self.toggleLegend = function () {
+    	self.showLegend(! self.showLegend());
+    	app.map.render('map');
+    };
+
+	// show bookmark stuff
+	self.showBookmarks = function (self, event) {
+		var $button = $(event.target),
+			$popover = $('#bookmark-popover');
+
+		if ($popover.is(":visible")) {
+			$popover.hide();
+		} else {
+			self.bookmarks.bookmarkName(null);
+			//TODO: move all this into bookmarks model
+			// hide the popover if already visible
+			$popover.show().position({
+				"my": "center bottom",
+				"at": "center top",
+				"of": $button
+			});
+
+		}
+	};
+
+
+	// show coords info in pointer
+	self.showPointerInfo = ko.observable(false);
+	self.togglePointerInfo = function () {
+		self.showPointerInfo(!self.showPointerInfo());
+	};
+
+
+	// load layers from fixture or the server
+	self.loadLayers = function (data) {
+
+		// load layers
+		$.each(data.layers, function (i, layer) {
+			var layerViewModel = new layerModel(layer);
+			self.layerIndex[layer.id] = layerViewModel;
+		});
+
+		// load themes
+		$.each(data.themes, function (i, theme) {
+			var layers = [];
+			$.each(theme.layers, function (j, layer_id) {
+				// create a layerModel and add it to the list of layers
+				layers.push(self.layerIndex[layer_id]);
+			});
+			self.themes.push(new themeModel(theme.name, layers));
+		});
+	};
+
+	// handle the search form
+	self.searchTerm = ko.observable();
+	self.layerSearch = function () {
+			
+	};
+
+	// do this stuff when the active layers change
+	self.activeLayers.subscribe(function () {
+		// initial index
+		var index = 300;
+		app.state.activeLayers = [];
+		$.each(self.activeLayers(), function (i, layer) {
+			// set the zindex on the openlayers layer
+			// layers at the beginning of activeLayers
+			// are above those that are at the end
+			// also save the layer state
+            app.setLayerZIndex(layer, index);
+			index--;
+		});
+
+		// update the url hash
+		app.updateUrl();
+	});
+    
+
+	return self;
+}
