9ac514d4d0f93f2c1652a1e0fafd77cfd0d32848
rhodges <rhodges@ecotrust.org>
2012-07-11 18:06:07 -0700

    adding add layer view

--- marco/data_viewer/views.py
+++ marco/data_viewer/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 import madrona.layer_manager as layer_manager
+from django.utils import simplejson
+from models import *
 
 def test(request):
 
@@ -19,8 +21,31 @@ def test(request):
 def create_layer(request):
     if request.method != 'POST':
 	return httpResponse('Action not permitted', status=403)
-    import pdb
-    pdb.set_trace()
+    try:
+	url = request.POST['url']
+	name = request.POST['name']
+	themes = request.POST.getlist('themes')
+	layer = layer_manager.models.Layer(
+	    url = url,
+	    name = name
+	)
+	layer.save()
+	theme_list = []
+	for theme_id in themes:
+	    theme = layer_manager.models.Theme.objects.get(id=theme_id)
+	    theme_list.append(theme)
+	layer.theme = theme_list
+	layer.save()
+    except Exception, e:
+        return HttpResponse(result + e.message, status=500)
+
+    result = {
+        "status_code":1,  
+        "success":True, 
+        "message":"Saved successfully",
+        "layer": layer.toDict
+    }              
+    return HttpResponse(simplejson.dumps(result))
 
 def update_layer(request, layer_id):
     if request.method != 'POST':
