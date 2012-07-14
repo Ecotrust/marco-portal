744bd5f2d600ae8fc70686583eb6b72cfd93db34
Scott Fletcher <sfletche@gmail.com>
2012-07-13 18:13:24 -0700

    and adding new viewer app

---
+++ marco/viewer/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+class Theme(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return unicode('%s' % (self.name))
+
+    @property
+    def toDict(self):
+        layers = [layer.id for layer in self.layer_set.filter(is_sublayer=False)]
+        themes_dict = {
+            'id': self.id,
+            'name': self.name,
+            'layers': layers,
+        }
+        return themes_dict
+
+class Layer(models.Model):
+    TYPE_CHOICES = (
+        ('XYZ', 'XYZ'),
+        ('WMS', 'WMS'),
+        ('radio', 'radio'),
+        ('Vector', 'Vector'),
+    )
+    name = models.CharField(max_length=100)
+    layer_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    sublayer = models.ManyToManyField('self', blank=True, null=True)
+    themes = models.ManyToManyField("Theme", blank=True, null=True)
+    is_sublayer = models.BooleanField(default=False)
+    legend = models.CharField(max_length=255, blank=True, null=True)
+    utfurl = models.CharField(max_length=255, blank=True, null=True)
+    
+    #data catalog links
+    bookmark = models.CharField(max_length=755, blank=True, null=True)
+    map_tiles = models.CharField(max_length=255, blank=True, null=True)
+    kml = models.CharField(max_length=255, blank=True, null=True)
+    data_download = models.CharField(max_length=255, blank=True, null=True)
+    metadata = models.CharField(max_length=255, blank=True, null=True)
+    source = models.CharField(max_length=255, blank=True, null=True)
+
+    def __unicode__(self):
+        return unicode('%s' % (self.name))
+
+    @property
+    def toDict(self):
+        sublayers = [
+            {
+                'name': layer.name,
+                'type': layer.layer_type,
+                'url': layer.url,
+                'utfurl': layer.utfurl,
+                'id': layer.id,
+                'parent': self.id,
+                'legend': layer.legend 
+            } 
+            for layer in self.sublayer.all()
+        ]
+        layers_dict = {
+            'name': self.name,
+            'type': self.layer_type,
+            'url': self.url,
+            'utfurl': self.utfurl,
+            'subLayers': sublayers,
+            'legend': self.legend,
+            'id': self.id
+        }
+        return layers_dict
---
+++ marco/viewer/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+from views import *
+
+urlpatterns = patterns('',
+    (r'^layer/([A-Za-z0-9_-]+)$', update_layer),
+    (r'^layer', create_layer),
+    (r'^get_json', getJson)
+)
