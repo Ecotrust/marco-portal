cc91bb1d0be1831b7cdedf7810411bb0c3258014
Edwin Knuth <eknuth@ecotrust.org>
2012-07-16 11:03:52 -0700

    adding tool links, updating sidebar

--- portal/wordpress/wp-content/themes/marco/sidebar.php
+++ portal/wordpress/wp-content/themes/marco/sidebar.php
@@ -1,5 +1,5 @@
 				<div id="sidebar1" class="fluid-sidebar sidebar span4" role="complementary">
-					<h3>Learn</h3>
+					<h3><a href="<?php echo get_bloginfo('wpurl'); ?>/learn">Learn</a></h3>
 					<ul class="unstyled">
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>/learn/administrative">Administrative</a></li>
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>/learn/conservation">Conservation</a></li>
@@ -10,12 +10,12 @@
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>/learn/recreation">Recreation</a></li>
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>/learn/maritime-industries">Maritime Industries</a></li>
 					</ul>
-					<h3>Explore</h3>
+					<h3><a href="<?php echo get_bloginfo('wpurl'); ?>/explore">Explore</a></h3>
 					<ul class="unstyled">
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>">Data Catalog</a></li>
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>">Data Priorities</a></li>
 					</ul>
-					<h3>Visualize</h3>
+					<h3><a href="<?php echo get_bloginfo('wpurl'); ?>/visualize">Visualize</a></h3>
 					<ul class="unstyled">
 						<li><a href="http://dev.marco.marineplanning.org">Planning Tool</a></li>
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>">Feature</a></li>
