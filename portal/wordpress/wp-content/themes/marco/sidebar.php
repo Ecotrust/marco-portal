d00d984ecd88cdb5cf7933a8fd646e369efd15c5
Scott Fletcher <sfletche@gmail.com>
2012-07-17 16:34:53 -0700

    reverse ordering of themes in the sidebar learn section.  also removing database dump from repo.

--- portal/wordpress/wp-content/themes/marco/sidebar.php
+++ portal/wordpress/wp-content/themes/marco/sidebar.php
@@ -3,14 +3,14 @@
 					<ul class="unstyled">
 						 <?php
 						// Set up the objects needed
-						$my_wp_query = new WP_Query();
-						$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+						//$my_wp_query = new WP_Query();
+						//$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
 
 						// Get the page as an Object
 						$learn =  get_page_by_title('Learn');
 
 						// Filter through all pages and find Learn's children
-						$learn_children = get_page_children($learn->ID, $all_wp_pages);
+						$learn_children = get_pages( array( 'child_of' => $learn->ID) );
 
 						echo "<ul class='styled'>";
 						foreach ($learn_children as $child) {
@@ -19,7 +19,7 @@
 	    				}
 	    				echo "</ul>";
 						// echo what we get back from WP to the browser
-						echo '<pre>'.print_r($learn_children,true).'</pre>';
+						//echo '<pre>'.print_r($learn_children,true).'</pre>';
 						?>
 					</ul>
 					<h3><a href="<?php echo get_bloginfo('wpurl'); ?>/explore">Explore</a></h3>
