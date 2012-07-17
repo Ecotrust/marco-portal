3654e3a6311088a72bbdc7149937239935ff8f89
Edwin Knuth <eknuth@ecotrust.org>
2012-07-17 12:16:09 -0700

    adding puppet/vagrant

---
+++ manifests/marco.pp
@@ -0,0 +1,61 @@
+
+#php5-mysql mysql-server libapache2-mod-php5
+
+# ensure that apt update is run before any packages are installed
+class apt {
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+  }
+
+  # Ensure apt-get update has been run before installing any packages
+  Exec["apt-update"] -> Package <| |>
+}
+
+include apt
+
+# install mysql server with the following root and wordpress passwords
+class mysql-server {
+  $root_password = "password"
+  $wordpress_password = "password"
+  package { "mysql-client": ensure => installed, require => Exec["apt-update"]}
+  package { "mysql-server": ensure => installed, require => Exec["apt-update"]}
+
+  exec { "Set MySQL server root password":
+    subscribe => [ Package["mysql-server"], Package["mysql-client"] ],
+    refreshonly => true,
+    unless => "mysqladmin -uroot -p$root_password status",
+    path => "/bin:/usr/bin",
+    command => "mysqladmin -uroot password $root_password",
+  }
+
+}
+
+include mysql-server
+
+# install some packages
+package { "php5-mysql":
+    ensure => "installed"
+}
+
+package { "libapache2-mod-php5":
+    ensure => "installed"
+
+}
+
+
+file {'/etc/apache2/conf.d/vhost.conf':
+  ensure => file,
+  content => template("vhost.erb"),
+  notify  => Service['apache2']
+}
+
+file {'/vagrant/portal/wordpress/wp-config.php':
+  ensure => file,
+  content => template("vhost.erb"),
+  notify  => Service['apache2']
+}
+
+service { 'apache2':
+    ensure => running,
+    subscribe => File["/etc/apache2/conf.d/vhost.conf"],
+}
--- portal/wordpress/wp-content/themes/marco/sidebar.php
+++ portal/wordpress/wp-content/themes/marco/sidebar.php
@@ -22,4 +22,18 @@
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>">Feature</a></li>
 						<li><a href="<?php echo get_bloginfo('wpurl'); ?>">Feature</a></li>
 					</ul>
+					 <?php
+					// Set up the objects needed
+					$my_wp_query = new WP_Query();
+					$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+
+					// Get the page as an Object
+					$portfolio =  get_page_by_title('Learn');
+
+					// Filter through all pages and find Learn's children
+					$portfolio_children = get_page_children($portfolio->ID, $all_wp_pages);
+
+					// echo what we get back from WP to the browser
+					echo '<pre>'.print_r($portfolio_children,true).'</pre>';
+					?>
 				</div>
