ab29db6bc9d7162a36502c3ab5134f2fb21f58c9
Edwin Knuth <eknuth@ecotrust.org>
2012-07-20 16:31:49 -0700

    wider page

--- portal/wordpress/wp-content/themes/marco/page.php
+++ portal/wordpress/wp-content/themes/marco/page.php
@@ -2,7 +2,7 @@
 			
 			<div id="content" class="clearfix row-fluid">
 			
-				<div id="main" class="span8 clearfix" role="main">
+				<div id="main" class="span9 clearfix" role="main">
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
