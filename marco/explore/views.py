0dc21df32168723523bdef0b02998454de6e0c60
Scott Fletcher <sfletche@gmail.com>
2012-07-20 12:21:02 -0700

    updating right column on catalog and needs pages with appropriate links

--- marco/explore/views.py
+++ marco/explore/views.py
@@ -5,8 +5,15 @@ from data_manager.models import *
 
 
 def data_catalog(request, template='catalog.html'):
+    from django.contrib.sites.models import Site
+    try:
+        domain = Site.objects.all()[0].domain
+        if 'localhost' in domain:
+            domain = 'http://localhost:8010'
+    except:
+        domain = '..'
     themes = Theme.objects.all().order_by('name')
-    context = {'themes': themes}
+    context = {'themes': themes, 'domain': domain}
     return render_to_response(template, RequestContext(request, context)) 
 
 def data_needs(request, template='needs.html'):
