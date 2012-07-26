1c5ccc14b9a72d4d6ef3048f491bee2c7353fca5
Scott Fletcher <sfletche@gmail.com>
2012-07-26 16:28:10 -0700

    adding framework for utfgrid attribution support. currently grabbing the utfgrids from the media directory...will need to experiment with proxy serving from aws a bit later...

--- media/marco-proto/map.js
+++ media/marco-proto/map.js
@@ -1,15 +1,18 @@
 app.init = function () {
 
+    //to turn basemap indicator off (hide the plus sign)
+    //see email from Matt on 7/26 2:24pm with list of controls
     var map = new OpenLayers.Map(null, {
         allOverlays: true,
-        displayProjection: new OpenLayers.Projection("EPSG:4326")
+        displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        projection: "EPSG:900913"
     });
     esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean Basemap", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
         sphericalMercator: true,
         isBaseLayer: true,
         numZoomLevels: 13
     });
-
+    
     map.addLayers([esriOcean]);
     esriOcean.setZIndex(100);
 
@@ -38,8 +41,8 @@ app.addLayerToMap = function(layer) {
         };
         if (layer.utfurl) {
             layer.utfgrid = new OpenLayers.Layer.UTFGrid({
-
                 url: layer.utfurl,
+                sphericalMercator: true,
                                  
                 utfgridResolution: 4, // default is 2
                 displayInLayerSwitcher: false,
@@ -50,9 +53,27 @@ app.addLayerToMap = function(layer) {
             
             layer.utfcontrol = new OpenLayers.Control.UTFGrid({
                 layers: [layer.utfgrid],
-                handlerMode: 'click',
+                handlerMode: 'hover',
                 callback: function(infoLookup) {
-                    console.dir(infoLookup);
+                    app.viewModel.attributeTitle(false);
+                    app.viewModel.attributeData(false);
+                    if (infoLookup) {
+                        var info, 
+                            msg;
+                        for (var idx in infoLookup) {
+                            info = infoLookup[idx];
+                            if (info && info.data) {                                     
+                                msg = info.data.OBJECTID;
+                            } else {
+                                app.viewModel.attributeTitle(false);
+                                app.viewModel.attributeData(false);
+                            }
+                        }
+                        if (info && info.data) {
+                            app.viewModel.attributeTitle('');
+                            app.viewModel.attributeData([{'display': 'ID: ', 'data': msg}]);
+                        }
+                    } 
                     //document.getElementById("info").innerHTML = msg;
                 }
             });
@@ -67,8 +88,8 @@ app.addLayerToMap = function(layer) {
                         sphericalMercator: true
                     }
                 )
-            );            
-            
+            );  
+            //app.addUTFAttribution(layer);
         } else if (layer.type == 'Vector') {
             layer.layer = new OpenLayers.Layer.Vector(
                 layer.name,
@@ -106,6 +127,35 @@ app.addLayerToMap = function(layer) {
     }
     layer.layer.opacity = layer.opacity();
     layer.layer.setVisibility(true);
+}
+
+app.addUTFAttribution = function(layer) {
+    app.map.events.register("mouseover", layer, function(e) {
+        //app.viewModel.attributeTitle(this.layer.name);
+        //app.viewModel.attributeData( [{'display': '', 'data': this.utfgrid.id}] ); 
+        var feature = this.layer.getFeatureById(e.target._featureId);
+        if ( feature ) {
+            debugger;
+            app.viewModel.attributeTitle(this.layer.name);
+            app.viewModel.attributeData( [{'display': '', 'data': this.utfgrid.id}] ); 
+        }
+        /*var feature = this.layer.getFeatureById(e.target._featureId);
+        if ( feature ) {
+            var attrs = this.attributes,
+                title = this.attributeTitle;
+            app.viewModel.attributeTitle(title);            
+            app.viewModel.attributeData($.map(attrs, function(attr) { 
+                return { 'display': attr.display, 'data': feature.data[attr.field] }; 
+            }));
+        }
+        return true;*/
+    });
+    
+    app.map.events.register("mouseout", layer.layer, function(e) {
+        app.viewModel.attributeTitle(false);
+        app.viewModel.attributeData(false);
+    });
+    
 }
 
 app.addAttribution = function(layer) {
