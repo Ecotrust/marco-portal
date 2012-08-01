e813d1113ca58521a3d9b235bcc1c3a0f5cc7f32
Scott Fletcher <sfletche@gmail.com>
2012-08-01 13:28:59 -0700

    adding basemaps along with new layerswitcher

--- media/marco-proto/map.js
+++ media/marco-proto/map.js
@@ -3,22 +3,52 @@ app.init = function () {
     //to turn basemap indicator off (hide the plus sign)
     //see email from Matt on 7/26 2:24pm with list of controls
     var map = new OpenLayers.Map(null, {
-        allOverlays: true,
+        //allOverlays: true,
         displayProjection: new OpenLayers.Projection("EPSG:4326"),
         projection: "EPSG:900913"
     });
-    esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean Basemap", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
+    esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
         sphericalMercator: true,
         isBaseLayer: true,
         numZoomLevels: 13
     });
+    openStreetMap = new OpenLayers.Layer.OSM("Open Street Map", "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png", {
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13
+    });
+    googleStreet = new OpenLayers.Layer.Google("Google Streets", {
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13
+    });
+    googleTerrain = new OpenLayers.Layer.Google("Google Physical", {
+        type: google.maps.MapTypeId.TERRAIN,
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13
+    });
+    googleSatellite = new OpenLayers.Layer.Google("Google Satellite", {
+        type: google.maps.MapTypeId.SATELLITE,
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13
+    });
+    nauticalCharts = new OpenLayers.Layer.WMS("Nautical Charts", "http://egisws02.nos.noaa.gov/ArcGIS/services/RNC/NOAA_RNC/ImageServer/WMSServer", 
+        {
+            layers: 'null'
+        },
+        {
+            isBaseLayer: true,
+            numZoomLevels: 13,
+            projection: "EPSG:3857"
+        }
+    );
     
-    map.addLayers([esriOcean]);
+    map.addLayers([esriOcean, openStreetMap, googleStreet, googleTerrain, googleSatellite, nauticalCharts]);
     esriOcean.setZIndex(100);
 
-    map.addControl(new OpenLayers.Control.LayerSwitcher( {
-        roundedCorner: true
-    }));
+    map.addControl(new SimpleLayerSwitcher());
     map.addControl(new OpenLayers.Control.ZoomBox( {
         //enables zooming to a given extent on the map by holding down shift key while dragging the mouse
     }));
@@ -69,6 +99,7 @@ app.addLayerToMap = function(layer) {
                 layer.name,
                 {
                     projection: new OpenLayers.Projection('EPSG:3857'),
+                    displayInLayerSwitcher: false,
                     strategies: [new OpenLayers.Strategy.Fixed()],
                     protocol: new OpenLayers.Protocol.HTTP({
                         url: layer.url,
