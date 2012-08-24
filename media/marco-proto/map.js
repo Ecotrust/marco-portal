ed459b4acc6e8b0cca91f2fce00b7744334e3c0f
Scott Fletcher <sfletche@gmail.com>
2012-08-23 17:23:30 -0700

    adding benthic habitat legend links into legend title. ensuring attributes are only output when provided for vector layers.

--- media/marco-proto/map.js
+++ media/marco-proto/map.js
@@ -77,7 +77,7 @@ app.init = function () {
     // callback functions for vector attribution (SelectFeature Control)
     var report = function(e) {
         var layer = e.feature.layer.layerModel;
-        if ( layer ) {
+        if ( layer.attributes.length ) {
             var attrs = layer.attributes,
                 title = e.feature.data[attrs[0].field],
                 text = [];
