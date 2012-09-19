958ff675282ad295dfd65da230ed637c239f6dfb
Edwin Knuth <eknuth@ecotrust.org>
2012-09-19 12:16:03 -0700

    pulling placeholder text for news pages

--- portal/wordpress/wp-content/themes/marco/page-news.php
+++ portal/wordpress/wp-content/themes/marco/page-news.php
@@ -15,7 +15,6 @@ Template Name: News
 						</div>
 						<div class="row-fluid">
 							<div class="span12">
-								At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
 							</div>
 						</div>
 						<div class="row-fluid bugs">
