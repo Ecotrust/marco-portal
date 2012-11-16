c52fc3eccc59367b9bd9b1c8827600550052f69a
Scott Fletcher <sfletche@gmail.com>
2012-11-16 14:43:29 -0800

    reistating socket url setting as placeholder and adding some code to the context building view to account for its absence

--- marco/visualize/views.py
+++ marco/visualize/views.py
@@ -5,5 +5,9 @@ import settings
 from data_manager.models import *
 
 def show_planner(request, template='planner.html'):
-    context = {'MEDIA_URL': settings.MEDIA_URL, 'SOCKET_URL': settings.SOCKET_URL, 'login': 'true'}
+    try:
+        socket_url = settings.SOCKET_URL
+    except AttributeError:
+        socket_url = ''
+    context = {'MEDIA_URL': settings.MEDIA_URL, 'SOCKET_URL': socket_url, 'login': 'true'}
     return render_to_response(template, RequestContext(request, context)) 
