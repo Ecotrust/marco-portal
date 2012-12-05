639def7684eb8acfc3275fe3f360a9e40c44963a
Scott Fletcher <sfletche@gmail.com>
2012-12-05 11:09:46 -0800

    adding active field to other admin display

--- marco/scenarios/admin.py
+++ marco/scenarios/admin.py
@@ -9,7 +9,7 @@ class ScenarioAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'user', 'active', 'date_created', 'date_modified')
     list_filter = ['date_modified', 'date_created']
     search_fields = ('name', 'user__username', 'id')
-    fields = ['name', 'description', 'user']#, 'input_objectives', 'support_file'] 
+    fields = ['name', 'active', 'description', 'user']#, 'input_objectives', 'support_file'] 
     #NOTE:  can't do 'input_parameters' because it manually specifies a 'through' model ('ScenarioParameters')
 admin.site.register(Scenario, ScenarioAdmin)
 
