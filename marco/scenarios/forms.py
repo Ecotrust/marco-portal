5f8f931d8cde849354b0cecfefc48fca7e6e7265
unknown <sfletcher@ecotrust.org>
2012-12-28 14:32:38 -0800

    fixing wind speed widget initial bg color activation

--- marco/scenarios/forms.py
+++ marco/scenarios/forms.py
@@ -41,8 +41,8 @@ class ScenarioForm(FeatureForm):
     # GeoPhysical
               
     input_parameter_depth = forms.BooleanField( widget=CheckboxInput(attrs={'class': 'parameters'}), required=False )
-    input_min_depth = forms.FloatField(initial=50, widget=forms.TextInput(attrs={'class':'slidervalue'}))
-    input_max_depth = forms.FloatField(initial=300, widget=forms.TextInput(attrs={'class':'slidervalue'}))
+    input_min_depth = forms.FloatField(initial=20, widget=forms.TextInput(attrs={'class':'slidervalue'}))
+    input_max_depth = forms.FloatField(initial=200, widget=forms.TextInput(attrs={'class':'slidervalue'}))
     input_depth = forms.FloatField( widget=DualSliderWidget('input_min_depth','input_max_depth',
                                                             min=10,max=500,step=10),
                                     required=False)
