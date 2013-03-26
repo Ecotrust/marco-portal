b07afb43fba0ede7f3c7cd477985ba4d4cdee136
unknown <sfletcher@ecotrust.org>
2013-03-26 16:00:43 -0700

    removing the icon-bar button from wordpress header until we are able to get it working properly...

--- portal/wordpress/wp-content/themes/marco/header.php
+++ portal/wordpress/wp-content/themes/marco/header.php
@@ -100,11 +100,12 @@
 					    </div>
 					    <div class="span7">
 					       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-					      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					      <!-- REMOVING THE FOLLOWING UNTIL WE FIND A SOLUTION -->
+                          <!--<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 					     	 <span class="icon-bar"></span>
 					      	 <span class="icon-bar"></span>
 					      	 <span class="icon-bar"></span>
-					      </a>
+					      </a>-->
 					      <div class="nav-collapse">
 					      	<div>
 						        <form class="form-search pull-right" action="<?php echo home_url( '/' ); ?>" method="get">
