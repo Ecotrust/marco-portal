66c4ee56f33af5b317d63a8b66c0a4c436a6dcd9
Edwin Knuth <eknuth@ecotrust.org>
2013-04-04 10:01:16 -0700

    adding ga stuff to explore and portal

--- portal/wordpress/wp-content/themes/marco/footer.php
+++ portal/wordpress/wp-content/themes/marco/footer.php
@@ -78,6 +78,20 @@
 
 		});
 		</script>
+
+		<!-- Google Analytics -->
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-35841423-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+		
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
   			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
