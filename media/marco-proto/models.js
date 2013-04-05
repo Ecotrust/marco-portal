be6cc50cb952cfc699b3fcf45a80e96eaa0ba760
unknown <sfletcher@ecotrust.org>
2013-04-05 14:38:50 -0700

    re-adjusting the feature attributes for Wind Planning Areas

--- media/marco-proto/models.js
+++ media/marco-proto/models.js
@@ -1668,13 +1668,13 @@ function viewModel() {
                 first = state.indexOf("Call"),
                 second = state.indexOf("WEA"),
                 third = state.indexOf("RFI");
-            if (first !== -1) {
+            /*if (first !== -1) {
                 state = state.slice(0, first);
             } else if (second !== -1) {
                 state = state.slice(0, second);
             } else if (third !== -1) {
                 state = state.slice(0, third);
-            }
+            }*/
             attrs.push({'display': '', 'data': state});
         } 
         return attrs;
