57bf988c4bc22ea660265a0aad601cfa679df2bf
unknown <sfletcher@ecotrust.org>
2013-05-21 16:53:02 -0700

    fixing issue in which data layers were not visible upon initial view of data panel from fullscreen.  also straightening out the text on the expandable map buttons.

--- media/marco-proto/models.js
+++ media/marco-proto/models.js
@@ -780,6 +780,7 @@ function viewModel() {
     // toggle layer panel visibility
     self.toggleLayers = function() {
         self.showLayers(!self.showLayers());
+        self.updateScrollBars();
         app.map.render('map');
         if (self.showLayers()) app.map.render('map'); //doing this again seems to prevent the vector wandering effect
         app.updateUrl();
