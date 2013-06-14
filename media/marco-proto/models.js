7044d3a116da61b9ef3c4c47e7909de6a6e6f1f1
unknown <sfletcher@ecotrust.org>
2013-06-14 14:23:34 -0700

    constructing legends from web services when applicable

--- media/marco-proto/models.js
+++ media/marco-proto/models.js
@@ -37,6 +37,46 @@ function layerModel(options, parent) {
         self.featureAttributionName = 'Party & Charter Boat Trips';
     } 
     
+    
+    // if legend is not provided, try using legend from web services 
+    if ( !self.legend && self.url && (self.arcgislayers !== -1) ) {
+        $.ajax({
+            dataType: "jsonp",
+            //http://ocean.floridamarine.org/arcgis/rest/services/SAFMC/SAFMC_Regulations/MapServer/legend/?f=pjson
+            url: self.url.replace('/export', '/legend/?f=pjson'),
+            type: 'GET',
+            success: function(data) {
+                if (data['layers']) {
+                    $.each(data['layers'], function(i, layerobj) {
+                        if (parseInt(layerobj['layerId'], 10) === parseInt(self.arcgislayers, 10)) {
+                            self.legend = {'elements': []};
+                            $.each(layerobj['legend'], function(j, legendobj) {
+                                //http://ocean.floridamarine.org/arcgis/rest/services/SAFMC/SAFMC_Regulations/MapServer/13/images/94ed037ab533027972ba3fc4a7c9d05c
+                                var swatchURL = self.url.replace('/export', '/'+self.arcgislayers+'/images/'+legendobj['url']),
+                                    label = legendobj['label'];
+                                if (label === "") {
+                                    label = layerobj['layerName'];
+                                }
+                                self.legend['elements'].push({'swatch': swatchURL, 'label': label});
+                                //console.log(self.legend);
+                            });
+                            debugger;
+                        }
+                    });
+                    //reset visibility (to reset activeLegendLayers)
+                    var visible = self.visible();
+                    self.visible(false);
+                    self.visible(visible);
+                } else {
+                    //debugger;
+                }
+            }, 
+            error: function(error) {
+                //debugger;
+            }
+        });
+    }
+    
     // set target blank for all links
     if (options.description) {
         $descriptionTemp = $("<div/>", {
